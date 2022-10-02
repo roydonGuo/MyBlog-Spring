eb4259637599cca59cad6ae2636a3abec02c0b50
roydonGuo <3133010060@qq.com>
2022-10-02 19:18:22 +0800

    启动类日志打印

--- Vlog/src/main/java/edu/zut/vlog/VlogApplication.java
+++ Vlog/src/main/java/edu/zut/vlog/VlogApplication.java
@@ -1,13 +1,20 @@
 package edu.zut.vlog;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class VlogApplication {
 
+    @Value("${server.port}")
+    private static String serverPort;
+
     public static void main(String[] args) {
         SpringApplication.run(VlogApplication.class, args);
+        log.info("项目启动中...localhost:{}",serverPort);
     }
 
 }
